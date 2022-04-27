40377053664bb31e061a90baaaea3a1d58d82ed5
Benjamin Cope <brc1379@gmail.com>
2022-04-27 11:49:16 -0700

    remove morgan from app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
 
 const app = express();
 const router = require('./server/routes');
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
 app.use('/api/fec2/rfp/reviews', router);
 const port = 3002;
 
