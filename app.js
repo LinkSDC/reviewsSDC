9a510d82d37b9dc057609440be958529fee41873
Benjamin Cope <brc1379@gmail.com>
2022-04-28 15:18:48 -0700

    added loaderio test tag

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use('/api/fec2/rfp/reviews', router);
 const port = 3002;
 
+app.get('/loaderio-7c51c99a567151ad844aad39e7375e6f/', (req, res) => {
+  res.send('loaderio-7c51c99a567151ad844aad39e7375e6f');
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
